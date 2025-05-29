582e336c99ae842126fff23ba7063f790fb3055e
mubaraktoye <toyemubarak@gmail.com>
2025-05-29 13:41:31 +0100

    Trnasaction checker Added

---
+++ backend/Utility/SniffingDog/Models/Transactions.cs
@@ -0,0 +1,11 @@
+﻿namespace backend.Utility.SniffingDog.Models
+{
+    public class Transactions
+    {
+        public string TransactionId { get; set; }
+        public string UserId { get; set; }
+        public string Category { get; set; } // e.g., "food", "transfer", "airtime"
+        public decimal Amount { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
